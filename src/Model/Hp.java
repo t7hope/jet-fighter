d2bf700b56c03f28594c7e9a61363a247fbadc12
Tam <vovuminhtam135@gmail.com>
2023-10-23 10:34:13 +0700

    Add Hp

---
+++ src/Model/Hp.java
@@ -0,0 +1,55 @@
+package Model;
+
+public class Hp {
+	int x;
+	int y;
+	double width;
+	int height;
+
+	public Hp(int x, int y, double width, int height) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	// decrease hp
+	public void decreaseWidth(double i) {
+		setWidth(width - i);
+		// System.out.println(width);
+	}
+
+}
